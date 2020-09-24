a1df85d237e43d7a1e10f784d1241c032e8d4401
kimhwan <khan@MacBook-Pro.local>
2020-09-24 15:37:30 +0900

    board 개발할것 list

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,6 +4,15 @@ const Board = () => {
   return (
     <div>
       <h1>공지사항 - 개발중...</h1>
+      <ul>
+        <li>1. Font 변경,</li>
+        <li>2. darkmode switch 왼쪽으로 보내기</li>
+        <li>3. Dash board 만들기</li>
+        <li>4. Login, signup 시 input에 auto focus</li>
+        <li>5. User component (or modal?)</li>
+        <li>6. Contact component</li>
+        <li>7. 공지사항 component - 댓글</li>
+      </ul>
     </div>
   );
 };
