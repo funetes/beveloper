ac41ad5243873755e1244d4d8061aa774f0cc0c5
kimhwan <khan@MacBook-Pro.local>
2020-10-08 17:53:12 +0900

    list update

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -6,13 +6,10 @@ const Board = () => {
       <h1>공지사항 - 개발중...</h1>
       <pre>
         {`
-        1. data fatching시 compoent가 unmount되었을떼 fatching cancel해야함
-        2. admin 관리 page
-        3. 공지사항 ( 좋아요 )
-        4. Dash board 만들기 - Complete - watched
-        5. Redux 사용하기
-        6. typescript 적용하기
-        7. darkmode localstorage에 저장하기
+        data fatching시 compoent가 unmount되었을떼 fatching cancel해야함
+        공지사항 ( 좋아요 )
+        Dash board 만들기 - Complete - watched
+        typescript 적용하기
         `}
       </pre>
     </main>
